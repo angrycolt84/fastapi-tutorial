068d5e0f574e57cb6dd9d1c99f0b6c584afe8dd0
TakeThatForDataFiz <johnlunz8@gmail.com>
2026-02-28 21:59:40 -0500

    added test changes

--- src/main.py
+++ src/main.py
@@ -1,153 +1,193 @@
-from typing import Annotated
-from fastapi import FastAPI, Request, Depends, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-from contextlib import asynccontextmanager
-# import time
-# from fastapi.middleware.cors import CORSMiddleware
+# from typing import Annotated
+# from fastapi import FastAPI, Request, Depends, HTTPException, Query
+# from sqlmodel import Field, Session, SQLModel, create_engine, select
+# from contextlib import asynccontextmanager
+# # import time
+# # from fastapi.middleware.cors import CORSMiddleware
 
-# app = FastAPI()
-
-
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:8080",
-# ]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-# @app.middleware("http")
-# async def add_process_time_header(request: Request, call_next):
-#     start_time = time.perf_counter()
-#     response = await call_next(request)
-#     process_time = time.perf_counter() - start_time
-#     response.headers["X-Process-Time"] = str(process_time)
-#     return response
-
-# @app.get("/")
-# async def main():
-#     return {"message": "Hello World"}
+# # app = FastAPI()
 
 
-# class Hero(SQLModel, table=True):
-#     id: int | None = Field(default=None, primary_key=True)
+# # origins = [
+# #     "http://localhost.tiangolo.com",
+# #     "https://localhost.tiangolo.com",
+# #     "http://localhost",
+# #     "http://localhost:8080",
+# # ]
+# # app.add_middleware(
+# #     CORSMiddleware,
+# #     allow_origins=origins,
+# #     allow_credentials=True,
+# #     allow_methods=["*"],
+# #     allow_headers=["*"],
+# # )
+# # @app.middleware("http")
+# # async def add_process_time_header(request: Request, call_next):
+# #     start_time = time.perf_counter()
+# #     response = await call_next(request)
+# #     process_time = time.perf_counter() - start_time
+# #     response.headers["X-Process-Time"] = str(process_time)
+# #     return response
+
+# # @app.get("/")
+# # async def main():
+# #     return {"message": "Hello World"}
+
+
+# # class Hero(SQLModel, table=True):
+# #     id: int | None = Field(default=None, primary_key=True)
+# #     name: str = Field(index=True)
+# #     age: int | None = Field(default=None, index=True)
+# #     secret_name: str
+
+# class HeroBase(SQLModel):
 #     name: str = Field(index=True)
 #     age: int | None = Field(default=None, index=True)
+
+# class Hero(HeroBase, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
 #     secret_name: str
 
-class HeroBase(SQLModel):
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
+# class HeroPublic(HeroBase):
+#     id: int
 
-class Hero(HeroBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    secret_name: str
+# class HeroCreate(HeroBase):
+#     secret_name: str
 
-class HeroPublic(HeroBase):
-    id: int
-
-class HeroCreate(HeroBase):
-    secret_name: str
-
-class HeroUpdate(HeroBase):
-    name: str | None = None
-    age: int | None = None
-    secret_name: str | None = None
+# class HeroUpdate(HeroBase):
+#     name: str | None = None
+#     age: int | None = None
+#     secret_name: str | None = None
 
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# sqlite_file_name = "database.db"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False}
+# connect_args = {"check_same_thread": False}
 
-engine = create_engine(url=sqlite_url, connect_args=connect_args)
+# engine = create_engine(url=sqlite_url, connect_args=connect_args)
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+# def create_db_and_tables():
+#     SQLModel.metadata.create_all(engine)
 
-def get_session():
-    with Session(engine) as session:
-        yield session
+# def get_session():
+#     with Session(engine) as session:
+#         yield session
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    yield
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     create_db_and_tables()
+#     yield
     
-SessionDep = Annotated[Session, Depends(get_session)]
+# SessionDep = Annotated[Session, Depends(get_session)]
 
-app = FastAPI(lifespan=lifespan)
+# app = FastAPI(lifespan=lifespan)
 
 
-# @app.post("/heroes/")
-# def create_hero(hero: Hero, session: SessionDep) -> Hero:
-#     session.add(hero)
+# # @app.post("/heroes/")
+# # def create_hero(hero: Hero, session: SessionDep) -> Hero:
+# #     session.add(hero)
+# #     session.commit()
+# #     session.refresh(hero)
+# #     return hero
+
+# @app.post("/heroes/", response_model=HeroPublic)
+# def create_hero(hero: HeroCreate, session: SessionDep):
+#     db_hero = Hero.model_validate(hero)
+#     session.add(db_hero)
 #     session.commit()
-#     session.refresh(hero)
-#     return hero
-
-@app.post("/heroes/", response_model=HeroPublic)
-def create_hero(hero: HeroCreate, session: SessionDep):
-    db_hero = Hero.model_validate(hero)
-    session.add(db_hero)
-    session.commit()
-    session.refresh(db_hero)
-    return db_hero
+#     session.refresh(db_hero)
+#     return db_hero
 
 
 
-@app.get("/heroes/")
-def read_heroes(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
-    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-    return list(heroes)
-
-# @app.get("/heroes/{hero_id}")
-# def read_single_hero(
-#     hero_id: int,
+# @app.get("/heroes/")
+# def read_heroes(
 #     session: SessionDep,
-# ) -> Hero:
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Hero]:
+#     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+#     return list(heroes)
+
+# # @app.get("/heroes/{hero_id}")
+# # def read_single_hero(
+# #     hero_id: int,
+# #     session: SessionDep,
+# # ) -> Hero:
+# #     hero = session.get(Hero, hero_id)
+# #     if not hero:
+# #         raise HTTPException(status_code=404, detail="No Hero Found")
+# #     return hero
+
+# @app.get("/heroes/{hero_id}", response_model=HeroPublic)
+# def read_single_hero(hero_id: int, session: SessionDep):
 #     hero = session.get(Hero, hero_id)
 #     if not hero:
 #         raise HTTPException(status_code=404, detail="No Hero Found")
 #     return hero
-
-@app.get("/heroes/{hero_id}", response_model=HeroPublic)
-def read_single_hero(hero_id: int, session: SessionDep):
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="No Hero Found")
-    return hero
     
 
-@app.delete("/heroes/{hero_id}")
-def delete_hero(
-    hero_id: int,
-    session: SessionDep,
-):
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="No Hero Found")
-    session.delete(hero)
-    session.commit()
-    return {"ok": True}
+# @app.delete("/heroes/{hero_id}")
+# def delete_hero(
+#     hero_id: int,
+#     session: SessionDep,
+# ):
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="No Hero Found")
+#     session.delete(hero)
+#     session.commit()
+#     return {"ok": True}
 
-@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
-def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
-    hero_db = session.get(Hero, hero_id)
-    if not hero_db:
-        raise HTTPException(status_code=404, detail="No Hero Found")
-    hero_data = hero.model_dump(exclude_unset=True)
-    hero_db.sqlmodel_update(hero_data)
-    session.add(hero_db)
-    session.commit()
-    session.refresh(hero_db)
-    return hero_db
+# @app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+# def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+#     hero_db = session.get(Hero, hero_id)
+#     if not hero_db:
+#         raise HTTPException(status_code=404, detail="No Hero Found")
+#     hero_data = hero.model_dump(exclude_unset=True)
+#     hero_db.sqlmodel_update(hero_data)
+#     session.add(hero_db)
+#     session.commit()
+#     session.refresh(hero_db)
+#     return hero_db
+
+
+from typing import Annotated
+
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
+
+fake_secret_token = "coneofsilence"
+
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+}
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    id: str
+    title: str
+    description: str | None = None
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_main(item_id: str, x_token: Annotated[str, Header()]):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return fake_db[item_id]
+
+
+@app.post("/items/")
+async def create_item(item: Item, x_token: Annotated[str, Header()]) -> Item:
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item.id in fake_db:
+        raise HTTPException(status_code=409, detail="Item already exists")
+    fake_db[item.id] = item.model_dump()
+    return item
